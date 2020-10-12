0ec11ec1e7e3aaa15d31ac1c961cd5b24871a05a
jiayuanfa <jiayuanfa@yeah.net>
2020-10-12 23:56:49 +0800

    调用摄像头、相册获取图片

--- app/src/main/java/com/example/datebasetestdemo/MainActivity.kt
+++ app/src/main/java/com/example/datebasetestdemo/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.datebasetestdemo
 
+import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.FileProvider
+import androidx.core.graphics.createBitmap
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.lang.Exception
 import java.lang.NullPointerException
 
@@ -22,10 +31,45 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "MainActivity"
     }
 
+    // 摄像头相关
+    private val takePhoto = 1
+    private val fromAlbum = 2
+    lateinit var imageUri: Uri
+    lateinit var  outputImage: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 从相册选择
+        albumButton.setOnClickListener {
+            // 打开文件选择器
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            // 指定只显示图片
+            intent.type = "image/*"
+            startActivityForResult(intent, fromAlbum)
+        }
+
+        // 调起摄像头
+        cameraButton.setOnClickListener {
+            // 创建File对象，暂存拍摄之后的图片
+            outputImage = File(externalCacheDir, "output_image.jpg")
+            if (outputImage.exists()) {
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            // 将File转化为Uri对象，注意版本
+            imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                FileProvider.getUriForFile(this, "com.example.camera.test", outputImage)
+            } else {
+                Uri.fromFile(outputImage)
+            }
+            // 启动相机程序
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, takePhoto)
+        }
 
         // 创建一个通知
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
@@ -137,5 +181,58 @@ class MainActivity : AppCompatActivity() {
                 db.endTransaction()
             }
         }
+    }
+
+    /**
+     * 监听相机回调
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            takePhoto -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    // 将拍照的结果取出来
+                    val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+                    mImageView.setImageBitmap(rotateIfRequired(bitmap))
+                }
+            }
+
+            fromAlbum -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        val bitmap = getBitmapFromUri(uri)
+                        mImageView.setImageBitmap(bitmap)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getBitmapFromUri(uri: Uri) = contentResolver
+            .openFileDescriptor(uri, "r")?.use {
+                BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
+            }
+
+    /**
+     * 如果需要旋转图片，先旋转图片
+     */
+    private fun rotateIfRequired(bitmap: Bitmap): Bitmap {
+        val exif = ExifInterface(outputImage.path)
+        val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+        ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
+            else -> bitmap
+        }
+    }
+
+    private fun rotateBitmap(bitmap: Bitmap, degree: Int): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotateBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        bitmap.recycle()
+        return rotateBitmap
     }
 }
